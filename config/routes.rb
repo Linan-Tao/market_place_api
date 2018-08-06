f48b911fbf1b1488dc86de0ec28dd73ecfa6a511
Linan-Tao <linan.avvo@gmail.com>
2018-08-06 21:45:25 +0800

    add route spec

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
+require 'api_constraints'
 Rails.application.routes.draw do
-  namespace :api, {format: :json} do
-    
+  namespace :api, defaults: {format: :json},
+                    constraints: { subdomian: 'api'}, path: '/' do
+    scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
+    end
   end
 end
